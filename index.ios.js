681f34144aa0dec2d6ee5a2444f8a4c15b25bd4a
Brent Vatne <brentvatne@gmail.com>
2015-06-09 19:23:40 -0700

    Refactor it into a class

--- index.ios.js
+++ index.ios.js
@@ -12,113 +12,106 @@ var {
   PanResponder,
 } = React;
 
+var { ReactPage, JestPage, FlowPage, } = require('./examples');
 var deviceWidth = require('Dimensions').get('window').width;
+var tweenState = require('react-tween-state');
 
-var ScrollFun = React.createClass({
-  mixins: [],
-
-  getInitialState: function() {
-    return {offsetX: 0}
+var ScrollableTabView = React.createClass({
+  getInitialState() {
+    return {offsetX: 0,}
   },
 
-  componentWillMount: function() {
+  componentWillMount() {
     this._panResponder = PanResponder.create({
+      // Claim responder if it's a horizontal pan
       onMoveShouldSetPanResponder: (e, gestureState) => {
         if (Math.abs(gestureState.dx) > Math.abs(gestureState.dy)) {
           this.setState({beginOffsetX: this.state.offsetX});
           return true;
-        } else {
-          return false;
         }
       },
 
+      // Touch is released, scroll to the one that you're closest to
       onPanResponderRelease: (e, gestureState) => {
         this.goToPage(this.gestureStateToPageNumber(gestureState));
       },
 
-      /* Here I want to tween to the new state */
+      // Dragging, move the view with the touch
       onPanResponderMove: (e, gestureState) => {
         this.setState({offsetX: this.state.beginOffsetX + gestureState.dx});
       },
     });
   },
 
-  gestureStateToPageNumber: function(gestureState) {
+  gestureStateToPageNumber(gestureState) {
     // A right->left drag means that you want to move the content towards
     // the left, and the resulting dx is negative, so let's invert it
     var dx = -1 * (this.state.beginOffsetX + gestureState.dx),
-        approxPageNumber = dx / deviceWidth;
+        approxPageNumber = dx / deviceWidth,
+        lastTabId = this.props.children.length - 1;
 
     // Overscroll to the left
     if (approxPageNumber < 0) {
       return 0;
-    } else if (approxPageNumber > 2) {
-      return 2;
+    } else if (approxPageNumber > lastTabId) {
+      return lastTabId;
     } else {
       return Math.round(approxPageNumber);
     }
   },
 
-  goToPage: function(pageNumber) {
+  goToPage(pageNumber) {
     this.setState({offsetX: -1 * (pageNumber * deviceWidth)});
   },
 
-  render: function() {
+  renderTabOption(name) {
     return (
-      <View style={styles.container}>
+      <TouchableOpacity key={name}>
+         <View style={styles.tab}>
+          <Text>{name}</Text>
+         </View>
+      </TouchableOpacity>
+    );
+  },
+
+  render() {
+    var numberOfTabs = this.props.children.length;
+    var tabUnderlineStyle = {
+      position: 'absolute',
+      width: deviceWidth / numberOfTabs,
+      left: -1 * (this.state.offsetX / numberOfTabs),
+      height: 10, backgroundColor: 'red', bottom: 0,
+    }
+    var sceneContainerStyle = {
+      width: deviceWidth * this.props.children.length,
+      flex: 1, flexDirection: 'row',
+      transform: [{translateX: this.state.offsetX}]
+    }
+
+    return (
+      <View style={{flex: 1}}>
         <View style={styles.tabs}>
-          <TouchableOpacity>
-             <View style={styles.tab}>
-              <Text>React</Text>
-             </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-             <View style={styles.tab}>
-              <Text>Flow</Text>
-             </View>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <View style={styles.tab}>
-              <Text>Jest</Text>
-            </View>
-          </TouchableOpacity>
-          <View style={{position: 'absolute', width: deviceWidth / 3, height: 10, backgroundColor: 'red', bottom: 0, left: -1 * (this.state.offsetX / 3)}} />
+          {this.props.children.map((child) => this.renderTabOption(child.key))}
+          <View style={tabUnderlineStyle} />
         </View>
 
-        <View style={{width: deviceWidth * 3, flex: 1, flexDirection: 'row', transform: [{translateX: this.state.offsetX}]}}
-              {...this._panResponder.panHandlers} >
-
-          <ScrollView style={styles.react}>
-            <Image source={{uri: "http://sc5.io/blog/wp-content/uploads/2014/06/react.png"}}
-                   style={{flex: 1, height: 320}} resizeMode="cover" />
-            <Text>
-              JUST THE UI Lots of people use React as the V in MVC. Since React makes no assumptions about the rest of your technology stack, it's easy to try it out on a small feature in an existing project. VIRTUAL DOM React abstracts away the DOM from you, giving a simpler programming model and better performance. React can also render on the server using Node, and it can power native apps using React Native. DATA FLOW React implements one-way reactive data flow which reduces boilerplate and is easier to reason about than traditional data binding.
-            </Text>
-            <Image source={{uri: "http://sc5.io/blog/wp-content/uploads/2014/06/react.png"}}
-                   style={{flex: 1, height: 320}} resizeMode="cover" />
-          </ScrollView>
-
-          <ScrollView>
-            <Image source={{uri: "http://www.adweek.com/socialtimes/files/2014/11/FlowLogo650.jpg"}}
-                   style={{flex: 1, height: 320}} resizeMode="contain" />
-
-            <Image source={{uri: "http://www.adweek.com/socialtimes/files/2014/11/FlowLogo650.jpg"}}
-                   style={{flex: 1, height: 320}} resizeMode="contain" />
-
-            <Image source={{uri: "http://www.adweek.com/socialtimes/files/2014/11/FlowLogo650.jpg"}}
-                   style={{flex: 1, height: 320}} resizeMode="contain" />
-          </ScrollView>
-
-          <ScrollView style={styles.jest}>
-            <Image source={{uri: "http://facebook.github.io/jest/img/opengraph.png"}}
-                   style={{flex: 1, height: 320}} resizeMode="cover" />
-            <Image source={{uri: "http://facebook.github.io/jest/img/opengraph.png"}}
-                   style={{flex: 1, height: 320}} resizeMode="cover" />
-            <Image source={{uri: "http://facebook.github.io/jest/img/opengraph.png"}}
-                   style={{flex: 1, height: 320}} resizeMode="cover" />
-          </ScrollView>
+        <View style={sceneContainerStyle} {...this._panResponder.panHandlers}>
+          {this.props.children}
         </View>
       </View>
+    )
+  }
+});
+
+
+var ScrollFun = React.createClass({
+  render() {
+    return (
+      <ScrollableTabView>
+        <ReactPage key="React" />
+        <FlowPage key="Flow" />
+        <JestPage key="Jest" />
+      </ScrollableTabView>
     );
   }
 });
@@ -141,13 +134,6 @@ var styles = StyleSheet.create({
     borderRightWidth: 0,
     borderBottomColor: '#ccc',
 
-  },
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
   },
 });
 
